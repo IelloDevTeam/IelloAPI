4b70f850ca8e10ddf2476ada7c1db0a178e61e7c
Andrea Petreti <petretiandrea@gmail.com>
2017-09-21 23:17:07 +0200

    Prova risposta API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var admin = require("firebase-admin");
+var express = require("express");
 
 var serviceAccount = require("./piattaforme-firebase-key.json");
+var app = express();
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
@@ -12,3 +14,25 @@ var ref = db.ref("/segnalazioni");
 ref.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
+
+app.get("/parking", function(req, res){
+	if(typeof req.query !== 'undefined')
+	{
+		if("radius" in req.query)
+			console.log("Raggio impostato: " + req.query.radius);
+		else
+			console.log("Raggio impostato di default");
+	
+		console.log(req.query);
+
+		if("lat" in req.query && "lon" in req.query)
+		{
+			console.log("Lat :" + req.query.lat);
+			console.log("Lon :" + req.query.lon); 
+		} else 
+			console.log("Latitudine e longitudie sono obbligatori");
+	}
+	res.send("ciao");
+});
+
+app.listen(3000, function(err){});
