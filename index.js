b64d3ab5705ef1cc00c4b1f5dd74166b69b83d1a
Andrea Petreti <petretiandrea@gmail.com>
2017-10-16 20:01:59 +0200

    Code Refactoring

--- index.js
+++ index.js
@@ -1,142 +1,14 @@
 const PORT = 4000;
-const RAGGIO_DEFAULT = 500; 	// 500 metri
-const LIMIT_DEFAULT = 20;	// Limite parcheggi 20
 
-var admin = require("firebase-admin");
 var express = require("express");
-
-// aggiunta libreria per il permitero
-var latlng = require("./selection-perimeter");
-// lib geocoding
-var geocoding = require("./maps-geocoding");
-
-var serviceAccount = require("./piattaforme-firebase-key.json");
 var app = express();
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://piattaforme-ca3e9.firebaseio.com/"
-});
+/* Richiesta controller parcheggi */
+var parkingController = require('./parking-controller');
 
-var db = admin.database();
-
-function checkParameterMiddleware(req, res, next)
-{
-	var raggio = RAGGIO_DEFAULT; //raggio di default
-	var limit = LIMIT_DEFAULT;
-	var lat = undefined;
-	var lon = undefined;
-
-	if(typeof req.query !== 'undefined')
-	{
-		// controllo parametri obbligatori
-		if("lat" in req.query && "lon" in req.query)
-		{
-			console.log("Lat :" + req.query.lat);
-			console.log("Lon :" + req.query.lon); 
-			if(req.query.lat.trim() && req.query.lon.trim() && !isNaN(req.query.lat) && !isNaN(req.query.lon))
-			{
-				lat = req.query.lat;
-				lon = req.query.lon;
-			} else
-				return sendResponseMessage(res, 400, "ERROR", "Invalid Latitude or Longitude Parameter");
-		} else
-			return sendResponseMessage(res, 400, "ERROR", "Missing Latitude or Longitude Parameter");
-
-		// controllo parametri opzionali
-		if("radius" in req.query)
-		{
-			if(!isNaN(req.query.radius) && req.query.radius.trim())
-			{
-				raggio = req.query.radius;			
-				console.log("Raggio impostato: " + req.query.radius);
-			}
-		}
-		else
-			console.log("Raggio impostato di default");
-
-		res.locals.data =  {
-			lat : lat,
-			lon : lon,
-			radius : raggio
-		};
-
-		next();
-	}
-}
-
-function filterParkingMiddleware(req, res, next)
-{
-	let latitudine = res.locals.data.lat;
-	let longitudine = res.locals.data.lon;
-	let raggio = res.locals.data.radius;
-
-	var coordinateLimite = latlng.perimetro(latitudine, longitudine, raggio);
-
-	var minLon = coordinateLimite[3];
-	var maxLon = coordinateLimite[1];
-	var minLat = coordinateLimite[2];
-	var maxLat = coordinateLimite[0];
-
-	var parking = [];
-
-	var ref = db.ref("/posti");
-
-	//console.log(data.radius);
-	//console.log("Min lon: " + minLon + " Max Lon: " + maxLon);
-	//console.log("Min lat: " + minLat + " Max Lat: " + maxLat);
-
-	ref.orderByChild("longitudine")
-			.startAt(minLon)
-			.endAt(maxLon)
-			.on("value", function(snapshot) {
-		// appena ricevo i dati rimuovo la callback.
-		ref.off();
-
-		// per ogni risultato filtro sulla latitudine
-		snapshot.forEach(function(child){
-			let parkLat = child.val().latitudine;
-			let parkLon = child.val().longitudine;
-			if(parkLat >= minLat && parkLat <= maxLat)
-			{
-				// aggiungo un parcheggio ai risultati
-				let dist = latlng.distance(latitudine, longitudine, parkLat, parkLon);
-				parking.push({
-					latitudine : parkLat,
-					longitudine : parkLon,
-					distance : Math.trunc((dist * 1000)),
-					street_address : child.val().street_address
-				});
-
-				/* Ordinamento in base alla distanza */
-				parking.sort(function(elem1, elem2){
-					return elem1.distance - elem2.distance;
-				});
-			}
-		});
-
-		sendResponseMessage(res, 200, "OK", {
-			"parking_count" : parking.length,
-			"parking" : parking
-		});
-
-	}, function(error){
-		console.log(error);
-		sendResponseMessage(res, 500, "ERROR", "Database Error");
-	});
-}
-
-function sendResponseMessage(res, httpCode, status, message)
-{
-	console.log("HTTP-Status: " + httpCode + " Status: " + status + " Message: " + message);
-	return res.status(httpCode).json({
-		status : status,
-		message : message
-	});
-}
-
-
-app.get("/parking", checkParameterMiddleware , filterParkingMiddleware);
+app.get("/parking",
+	parkingController.checkParameter,
+	parkingController.getAvailableParking);
 
 app.listen(PORT, function(err){
 	console.log("Pronto sulla porta " + PORT);
---
+++ parking-controller.js
@@ -0,0 +1,134 @@
+/* Costanti */
+const RAGGIO_DEFAULT = 500; 	// 500 metri
+const LIMIT_DEFAULT = 20;	// Limite parcheggi 20
+
+var exports = module.exports = {}
+
+/* Richiesta della libreria per i calcoli sulle coordinate */
+let coordUtil 	= require("./coord-util");
+/* Richiesta della librerira per il geocoding */
+let geocoding 	= require("./maps-geocoding");
+let admin 		= require("firebase-admin");
+
+admin.initializeApp({
+	credential: admin.credential.cert(require("./piattaforme-firebase-key.json")),
+	databaseURL: "https://piattaforme-ca3e9.firebaseio.com/"
+});
+
+/* Firebase Database */
+let db = admin.database();
+
+/* Funzione middleware per controllare i parametri ricevuti */
+exports.checkParameter = function(req, res, next)
+{
+	var raggio = RAGGIO_DEFAULT; //raggio di default
+	var limit = LIMIT_DEFAULT;
+	var lat = undefined;
+	var lon = undefined;
+
+	if(typeof req.query !== 'undefined')
+	{
+		// controllo parametri obbligatori
+		if("lat" in req.query && "lon" in req.query)
+		{
+			console.log("Lat :" + req.query.lat);
+			console.log("Lon :" + req.query.lon); 
+			if(req.query.lat.trim() && req.query.lon.trim() && !isNaN(req.query.lat) && !isNaN(req.query.lon))
+			{
+				lat = req.query.lat;
+				lon = req.query.lon;
+			} else
+				return sendResponseMessage(res, 400, "ERROR", "Invalid Latitude or Longitude Parameter");
+		} else
+			return sendResponseMessage(res, 400, "ERROR", "Missing Latitude or Longitude Parameter");
+
+		// controllo parametri opzionali
+		if("radius" in req.query)
+		{
+			if(!isNaN(req.query.radius) && req.query.radius.trim())
+			{
+				raggio = req.query.radius;			
+				console.log("Raggio impostato: " + req.query.radius);
+			}
+		}
+		else
+			console.log("Raggio impostato di default");
+
+		res.locals.data =  {
+			lat : lat,
+			lon : lon,
+			radius : raggio
+		};
+
+		next();
+	}
+}
+
+/* Funzione middleware per inviare la lista dei parcheggi disponibili */
+exports.getAvailableParking = function(req, res, next)
+{
+	/* Recupero dati validati da checkParameter */
+	let latitudine = res.locals.data.lat;
+	let longitudine = res.locals.data.lon;
+	let raggio = res.locals.data.radius;
+
+	/* Calcolo cordinate limite per un certo raggio */
+	let coordinateLimite = coordUtil.perimetro(latitudine, longitudine, raggio);
+
+	var minLon = coordinateLimite[3];
+	var maxLon = coordinateLimite[1];
+	var minLat = coordinateLimite[2];
+	var maxLat = coordinateLimite[0];
+
+	let parking = [];
+
+	/* Richesta parcheggi disponibili su firebase */
+	db.ref("/posti").orderByChild("longitudine")
+			.startAt(minLon)
+			.endAt(maxLon)
+			.once("value", function(snapshot) {
+
+		// per ogni risultato filtro sulla latitudine
+		snapshot.forEach(function(child){
+			let parkLat = child.val().latitudine;
+			let parkLon = child.val().longitudine;
+			if(parkLat >= minLat && parkLat <= maxLat)
+			{
+				// aggiungo un parcheggio ai risultati
+				// calcolo distanza tra il parcheggio e la posizione dell'utente
+				let dist = coordUtil.distance(latitudine, longitudine, parkLat, parkLon);
+				parking.push({
+					latitudine : parkLat,
+					longitudine : parkLon,
+					distance : Math.trunc((dist * 1000)),
+					street_address : child.val().street_address
+				});
+
+				// Ordinamento in base alla distanza
+				parking.sort(function(elem1, elem2){
+					return elem1.distance - elem2.distance;
+				});
+			}
+		});
+
+		sendResponseMessage(res, 200, "OK", {
+			"parking_count" : parking.length,
+			"parking" : parking
+		});
+
+	}, function(error){
+		console.log(error);
+		sendResponseMessage(res, 500, "ERROR", "Database Error");
+	});
+}
+
+/* Funzione per inviare una risposta HTTP */
+function sendResponseMessage(res, httpCode, status, message)
+{
+	console.log("HTTP-Status: " + httpCode + " Status: " + status + " Message: " + message);
+	return res.status(httpCode).json({
+		status : status,
+		message : message
+	});
+}
+
